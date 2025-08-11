67177244c2eef598a5d0af790ce8c3a563bdfed2
PiyushKalyanpy <kalyanpiyushp@gmail.com>
2025-08-11 12:28:49 +0530

    updated home page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,21 @@
-import { Geist_Mono, Manrope } from "next/font/google";
+import { Geist_Mono, Instrument_Serif, Inter_Tight, Manrope } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 
-const mainFont = Manrope({
+const mainFont = Inter_Tight({
   variable: "--font-geist-sans",
   subsets: ["latin"],
   weight: ["200", "300", "400", "500", "600", "700", "800"],
   style: ["normal"],
 });
+
+const serif = Instrument_Serif({
+  variable: "--font-intrument-serif",
+  subsets: ["latin"], 
+  weight:['400']
+});
+
+
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -23,7 +31,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${mainFont.className}   border-x  border-gray-100  mx-auto w-full min-h-[100dvh]  antialiased`}
+        className={`${mainFont.className} ${geistMono.variable} ${serif.variable}  border-x  border-gray-100  mx-auto w-full min-h-[100dvh]  antialiased`}
       >
         <Providers>
           <div className=" mx-auto w-full min-h-[100dvh] flex flex-col">
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,39 @@
 "use client";
 
 import { Button } from "@heroui/react";
+import { useRouter } from "next/navigation";
 
 export default function MainPage() {
+  const router = useRouter();
   return (
     <div className="full-wrapper">
-      <Button as="a" href="/login" color="primary">
+       <div
+      className="flex flex-col items-center justify-center w-full min-h-screen text-white bg-center bg-cover"
+      style={{
+        backgroundImage:
+          "url('https://img.freepik.com/free-photo/aerial-view-beautiful-sky-road-top-mountains-with-green-jungle-nan-province-thailand_335224-1063.jpg?t=st=1754894765~exp=1754898365~hmac=7d834cfb16b0444b9211efab1cec4d602c5340e0b4ecd3ced2157ef1b37ccd8b&w=2000')",
+      }}
+    >
+      {/* Overlay for better text readability */}
+      <div className="absolute inset-0 bg-black/20 bg-opacity-60"></div>
+
+      {/* Content container */}
+      <div className="relative z-10 px-6 text-center">
+        <h2 className="mb-2 text-sm font-semibold tracking-wide text-yellow-300 uppercase md:text-2xl">
+          Site is under construction
+        </h2>
+
+        <h1 className="mb-6 text-4xl font-medium md:text-5xl ">Welcome to <span className="font-serif italic ">Athithya</span></h1>
+
+      <Button 
+      size="lg"
+      onPress={()=> router.push("/login")}
+       className="text-white rounded-full bg-black/40 backdrop-blur-md"
+      >
         Login
       </Button>
-      <Button as="a" href="/register" color="primary">
-        SignUp
-      </Button>{" "}
-      <Button as="a" href="/create-profile" color="primary">
-        Create Profile
-      </Button>
+      </div>
+    </div>
     </div>
   );
 }
